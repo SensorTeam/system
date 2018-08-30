ed8cd4ef94d1e3258288af4523d3fc037fa331bc
Carlos Melegrito <cj@mlgrto.com>
2018-08-30 14:20:37 +1000

    Add basic training script

---
+++ train.py
@@ -0,0 +1,17 @@
+import config
+
+import classifier
+
+# Functions
+# -----------------------------
+
+def main():
+	print('TRAINING...')
+	classifier.train()
+	print('TRAINING COMPLETE')
+
+# Start the show...
+# -----------------------------
+
+if __name__ == '__main__':
+	main()
