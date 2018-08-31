1603131137f69b02d3cd2de679a9fe195c7bedf7
Carlos Melegrito <cj@mlgrto.com>
2018-08-31 15:06:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+mv scanner-mk* scanner
+mv finder-mk* finder
+mv classifier-mk* classifier
+
 pip install -r scanner/requirements.txt
 pip install -r finder/requirements.txt
 pip install -r classifier/requirements.txt
@@ -5,5 +9,6 @@ pip install -r classifier/requirements.txt
 mkdir input
 mkdir input/images
 mkdir input/training
+mkdir input/csv
 mkdir output
 
